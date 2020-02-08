e4913190b92217cd2dd45a81ba1e47de6368d04b
Tarun215 <tarun.cer18@itbhu.ac.in>
2020-02-09 01:29:22 +0530

    removed password hash and jwt tokens from response object

--- src/models/user.js
+++ src/models/user.js
@@ -60,6 +60,16 @@ userSchema.methods.genAuthToken = async function() {
     return token
 }
 
+userSchema.methods.toJSON = function () {
+    const user = this 
+    const userObject = user.toObject()
+    
+    delete userObject.password
+    delete userObject.tokens
+
+    return userObject
+}
+
 userSchema.statics.findByCredentials = async (email, password) => {
     const user = await User.findOne({ email })
     if(!user){
